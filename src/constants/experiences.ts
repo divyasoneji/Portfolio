2da2232f0a149776c77d9a6ca88282ff15102fc2
Divya Soneji <divya.soneji@hotmail.com>
2025-06-20 17:41:05 -0700

    Fix Experience Order

--- src/constants/experiences.ts
+++ src/constants/experiences.ts
@@ -6,13 +6,14 @@ export const experiences = [
     iconBg: "var(--pastel-pink)",
     icon: "/assets/fugro-logo.png",
     points: [
-      "Developed and maintained scalable front-end components using React and TypeScript.",
-      "Developed and maintained scalable backend services in .NET and C# to support high-volume LiDAR data processing and visualization.",
-      "Designed and optimized data models for structured storage, improving retrieval efficiency for large datasets.",
       "Automated CI/CD pipelines using GitHub Actions, streamlining deployment and testing workflows across multiple projects.",
-      "Built and maintained RESTful APIs for seamless integration between web applications and backend data processing services, ensuring cross-platform consistency.",
-      "Worked alongside UX/UI designers to improve functionality, optimize interactions, and deliver seamless user experience.",
       "Debugged and resolved high-priority, mission-critical backend issues, to ensure reliability.",
+      "Developed and maintained scalable front-end components using React & TypeScript.",
+      "Worked alongside UX/UI designers to improve functionality, optimize interactions, and deliver seamless user experience.",
+      "Developed and maintained scalable backend services using .NET & C# to support high-volume LiDAR data processing and visualization.",
+      "Designed and optimized data models for structured storage, improving retrieval efficiency for large datasets.",
+      "Built and maintained RESTful APIs for seamless integration between web applications and backend data processing services, ensuring cross-platform consistency.",
+      "Implemented and enhanced ETL data pipelines for automated acquisition and approval workflows, improving operational efficiency.",
     ],
   },
   {
@@ -24,7 +25,6 @@ export const experiences = [
     points: [
       "Increased code coverage by over 25% with NUnit tests (C#).",
       "Created and maintained API documentation, defining endpoint parameters, request/response formats, and integration guidelines.",
-      "Implemented and enhanced ETL data pipelines for automated acquisition and approval workflows, improving operational efficiency.",
       "Worked with relational and NoSQL databases, designing efficient data storage solutions to support business operations.",
       "Practiced Agile & Scrum methodologies, participating in sprint planning, daily stand-ups, and retrospectives to drive continuous improvement.",
     ],
